17aa15fd09826abb3f3774a65b400575bd74f90e
Josh Parrish <jparrish@astate.edu>
2015-12-31 18:37:34 -0600

    rm docs

--- files/piano_lab_dock.sh
+++ files/piano_lab_dock.sh
@@ -7,5 +7,4 @@ sleep 2
 $DOCKUTIL --add '/Applications/Launchpad.app' --no-restart
 $DOCKUTIL --add '/Applications/Firefox.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
-$DOCKUTIL --add '~/Documents' --no-restart
 $DOCKUTIL --add '~/Downloads'
