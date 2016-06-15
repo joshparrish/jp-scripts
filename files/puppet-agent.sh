cccace4aca4d6f186fe3d469b818076b741df012
Josh Parrish <jparrish@astate.edu>
2016-06-15 11:53:05 -0500

    fisrt commit

---
+++ files/puppet-agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Waiting for network access"
+/usr/sbin/scutil -w State:/Network/Global/DNS -t 180
+sleep 5
+
+# Get the serial number
+serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
+# If this is a VM in VMWare, Parallels, or Virtual Box, it might have weird serial numbers that Puppet doesn't like, so change it to something static
+if [[ `system_profiler SPHardwareDataType | grep VMware` || `system_profiler SPHardwareDataType | grep VirtualBox` || `system_profiler SPEthernetDataType | grep "/0x1ab8/"` ]]; then
+		# Remove any silly + or / symbols
+			serial="${serial//[+\/]}"
+		fi
+
+		/usr/sbin/scutil --set HostName "cfa-$serial-m"
+		/usr/sbin/scutil --set LocalHostName "cfa-$serial-m"
+		/usr/sbin/scutil --set ComputerName "cfa-$serial-m"
+
+		
+		/opt/puppetlabs/bin/puppet config set --server cfaserv.astate.edu 
+		/opt/puppetlabs/bin/puppet config set --environment class 
+		/opt/puppetlabs/bin/puppet agent -t --waitforcert 60 >> /var/log/puppetagent.log
