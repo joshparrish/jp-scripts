66ea1584bfc489d290781713696e14e3984293d6
Josh Parrish <jparrish@astate.edu>
2016-06-17 10:25:48 -0500

    fix

--- files/disable_auto_8021x.sh
+++ files/disable_auto_8021x.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 user="$(whoami)"
-su - user defaults -currentHost write com.apple.network.eapolcontrol EthernetAutoConnect -bool false
+su - $user -c "defaults -currentHost write com.apple.network.eapolcontrol EthernetAutoConnect -bool false"
 
