baa2a9794acec22c88b0f7eb42c0f35fc92f62d0
Josh Parrish <jparrish@astate.edu>
2016-06-17 10:37:37 -0500

    add sudo

--- files/disable_auto_8021x.sh
+++ files/disable_auto_8021x.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 user="$(whoami)"
-su - $user -c "defaults -currentHost write com.apple.network.eapolcontrol EthernetAutoConnect -bool false"
+sudo su - $user -c "defaults -currentHost write com.apple.network.eapolcontrol EthernetAutoConnect -bool false"
 
