a1235a1fd1a71a4717baba6451b63d0b802a7658
Josh Parrish <jparrish@astate.edu>
2016-08-12 11:17:55 -0500

    add class dockutil

---
+++ files/class_dock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKUTIL=/usr/local/bin/dockutil
+
+$DOCKUTIL --remove all --no-restart
+$DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
+$DOCKUTIL --add '/Applications/Safari.app' --no-restart
+$DOCKUTIL --add '/Applications/Calendar.app' --no-restart
+$DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
+$DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart
+$DOCKUTIL --add '/Applications/Microsoft PowerPoint.app' --no-restart
+$DOCKUTIL --add '/Applications/DVD Player.app' --no-restart
+$DOCKUTIL --add '~/Documents' --no-restart
--- files/run_rlm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-osascript -e 'do shell script "open -a Terminal /usr/local/run_rlm.command"'
