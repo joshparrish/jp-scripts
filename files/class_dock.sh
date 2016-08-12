36fe145c68b53eb45590beae8571fdf35ed41be0
Josh Parrish <jparrish@astate.edu>
2016-08-12 11:20:24 -0500

    added firefox and downloads

--- files/class_dock.sh
+++ files/class_dock.sh
@@ -5,9 +5,11 @@ DOCKUTIL=/usr/local/bin/dockutil
 $DOCKUTIL --remove all --no-restart
 $DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
 $DOCKUTIL --add '/Applications/Safari.app' --no-restart
+$DOCKUTIL --add '/Applications/Firefox.app' --no-restart
 $DOCKUTIL --add '/Applications/Calendar.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft PowerPoint.app' --no-restart
 $DOCKUTIL --add '/Applications/DVD Player.app' --no-restart
 $DOCKUTIL --add '~/Documents' --no-restart
+$DOCKUTIL --add '~/Downloads' --no-restart
