8a80d0bb4c7f7a822a9e408e40a6ee3533ecef0c
Josh Parrish <jparrish@astate.edu>
2016-08-17 14:39:05 -0500

    changed to audition

--- files/theatre_lab_dock.sh
+++ files/theatre_lab_dock.sh
@@ -7,7 +7,7 @@ $DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
 $DOCKUTIL --add '/Applications/Safari.app' --no-restart
 $DOCKUTIL --add '/Applications/Adobe Photoshop CC 2015.5/Adobe Photoshop CC 2015.5.app' --no-restart
 $DOCKUTIL --add '/Applications/Adobe Illustrator CC 2015.3/Adobe Illustrator.app' --no-restart
-$DOCKUTIL --add '/Applications/Adobe InDesign CC 2015/Adobe InDesign CC 2015.app' --no-restart
+$DOCKUTIL --add '/Applications/Adobe Audition CC 2015.2/Adobe Audition CC 2015.app' --no-restart
 $DOCKUTIL --add '/Applications/TextWrangler.app' --no-restart
 $DOCKUTIL --add '/Applications/QLab.app' --no-restart
 $DOCKUTIL --add '/Applications/Vista V2/Byron.app' --no-restart
