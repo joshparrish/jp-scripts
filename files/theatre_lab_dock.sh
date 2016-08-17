21d81bdf7f9df406e53243dff3021ae00f647138
Josh Parrish <jparrish@astate.edu>
2016-08-16 22:10:50 -0500

    edit lab dock

--- files/theatre_lab_dock.sh
+++ files/theatre_lab_dock.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-
 DOCKUTIL=/usr/local/bin/dockutil
 
 $DOCKUTIL --remove all --no-restart
 $DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
 $DOCKUTIL --add '/Applications/Safari.app' --no-restart
 $DOCKUTIL --add '/Applications/Adobe Photoshop CC 2015.5/Adobe Photoshop CC 2015.5.app' --no-restart
-$DOCKUTIL --add '/Applications/Adobe Photoshop CC 2015.5/Adobe Photoshop CC 2015.5.app' --no-restart
+$DOCKUTIL --add '/Applications/Adobe Illustrator CC 2015.5/Adobe Illustrator CC 2015.5.app' --no-restart
 $DOCKUTIL --add '/Applications/Adobe InDesign CC 2015.5/Adobe InDesign CC 2015.5.app' --no-restart
 $DOCKUTIL --add '/Applications/TextWrangler.app' --no-restart
 $DOCKUTIL --add '/Applications/QLab.app' --no-restart
-$DOCKUTIL --add '/Applications/SketchUp.app' --no-restart
 $DOCKUTIL --add '/Applications/Vista V2/Byron.app' --no-restart
-$DOCKUTIL --add '/Applications/Vectorworks 2015/Vectorworks 2015' --no-restart
+$DOCKUTIL --add '/Applications/Vectorworks 2015/Vectorworks 2015.app' --no-restart
 $DOCKUTIL --add '/Applications/VLC.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart
