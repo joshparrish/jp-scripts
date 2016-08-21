e56d1c1b159760ec7eef5a5da6316de12fd2e806
Josh Parrish <jparrish@astate.edu>
2016-08-21 15:59:53 -0500

    chg to chrome

--- files/piano_lab_dock.sh
+++ files/piano_lab_dock.sh
@@ -5,6 +5,6 @@ DOCKUTIL=/usr/local/bin/dockutil
 $DOCKUTIL --remove all --no-restart
 sleep 2
 $DOCKUTIL --add '/Applications/Launchpad.app' --no-restart
-$DOCKUTIL --add '/Applications/Firefox.app' --no-restart
+$DOCKUTIL --add '/Applications/Chrome.app' --no-restart
 $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
 $DOCKUTIL --add '~/Downloads'
